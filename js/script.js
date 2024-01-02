b6b3392e8d793846d6672720efc0d4c26f80a119
unknown <frankins636@gmail.com>
2024-01-02 16:46:35 +0200

    all pages

--- js/script.js
+++ js/script.js
@@ -87,8 +87,26 @@ swichLang.addEventListener("click", function(e) {
     toggleList.classList.toggle('lang_active');
 });
 
+// Переключить услугу на странице Услуги
++function () {
 
-
+    document.querySelector('.tab').classList.add('active')
+    document.querySelector('.tab-panel').classList.add('active')
+    
+    function selectPanel (e) {
+        var target = e.target.dataset.target
+    
+        document.querySelectorAll('.tab, .tab-panel').forEach(el => el.classList.remove('active'))
+        e.target.classList.add('active')
+        document.querySelector('.' + target)
+        document.querySelector('.' + target).classList.add('active')
+    }
+    
+    document.querySelectorAll('.tab').forEach(el => {
+        el.addEventListener('click', selectPanel)
+    })
+    
+    }()
 
 
 
